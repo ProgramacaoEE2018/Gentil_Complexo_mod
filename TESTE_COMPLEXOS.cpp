14af7dd4e76e28e3a55127e4e4b00d4dd6c4fec6
GentilGustavo <gustavogentil44@gmail.com>
2018-05-11 18:35:18 -0300

    first commit

---
+++ TESTE_COMPLEXOS.cpp
@@ -0,0 +1,70 @@
+#include "Complexo.h"
+#include <stdio.h>
+
+
+int main() {
+	Complexo z1(3, 4);
+	Complexo z2(3, 4);
+	Complexo conjugado(3, -4);
+	Complexo divisao(1, 0);
+	Complexo produto(-7, 24);
+	Complexo soma(6, 8);
+	float modulo;
+
+	//Teste Modulo
+	modulo = z1.Modulo();
+	if (modulo == 5) {
+		printf("Modulo OK\n");
+
+		//Teste Diferenca
+		modulo = (z1.Diferenca(z2)).Modulo();
+		if (modulo == 0) {
+			printf("Diferenca OK\n");
+		}
+		else {
+			printf("Falha Difernca\n");
+		}
+
+		//Teste Divisao
+		modulo = ((z1.Divisao(z2)) - divisao).Modulo();
+		if (modulo == 0) {
+			printf("Divisao OK\n");
+		}
+		else {
+			printf("Falha Divisao\n");
+		}
+
+		//Teste Conjugado
+		modulo = (z1.Conjugado() - conjugado).Modulo();
+		if (modulo == 0) {
+			printf("Conjugado OK\n");
+		}
+		else {
+			printf("Falha Conjugado\n");
+		}
+
+		//Teste Produto
+		modulo = ((z1.Produto(z2)) - produto).Modulo();
+		if (modulo == 0) {
+			printf("produto OK\n");
+		}
+		else {
+			printf("Falha produto\n");
+		}
+
+		//Teste Soma
+		modulo = ((z1.Soma(z2)) - soma).Modulo();
+		if (modulo == 0) {
+			printf("Soma OK\n");
+		}
+		else {
+			printf("Falha Soma\n");
+		}
+	}
+	else {
+		printf("Falha Modulo\n");
+	}
+
+
+	return 0;
+}
